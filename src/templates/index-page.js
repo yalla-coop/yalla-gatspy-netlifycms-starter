49548082ab077547250402e0b87b92b908665bb1
fadeomar <fadeomar2015@gmail.com>
2020-02-26 12:57:49 +0200

    create index page template

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,7 +1,93 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
 
-const Index = () => {
-  return <h1>Hi</h1>;
+export const IndexPageTemplate = ({
+  image,
+  title,
+  heading,
+  subheading,
+  mainpitch,
+  description,
+  intro
+}) => (
+  <div>
+    <h1>{title || "hi"}</h1>
+  </div>
+);
+
+// IndexPageTemplate.propTypes = {
+//   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+//   title: PropTypes.string,
+//   heading: PropTypes.string,
+//   subheading: PropTypes.string,
+//   mainpitch: PropTypes.object,
+//   description: PropTypes.string,
+//   intro: PropTypes.shape({
+//     blurbs: PropTypes.array,
+//   }),
+// }
+
+const IndexPage = ({ data }) => {
+  // const { frontmatter } = data.markdownRemark
+
+  return (
+    <IndexPageTemplate
+    // image={frontmatter.image}
+    // title={frontmatter.title}
+    // heading={frontmatter.heading}
+    // subheading={frontmatter.subheading}
+    // mainpitch={frontmatter.mainpitch}
+    // description={frontmatter.description}
+    // intro={frontmatter.intro}
+    />
+  );
 };
 
-export default Index;
+// IndexPage.propTypes = {
+//   data: PropTypes.shape({
+//     markdownRemark: PropTypes.shape({
+//       frontmatter: PropTypes.object,
+//     }),
+//   }),
+// }
+
+export default IndexPage;
+
+// export const pageQuery = graphql`
+//   query IndexPageTemplate {
+//     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+//       frontmatter {
+//         title
+//         image {
+//           childImageSharp {
+//             fluid(maxWidth: 2048, quality: 100) {
+//               ...GatsbyImageSharpFluid
+//             }
+//           }
+//         }
+//         heading
+//         subheading
+//         mainpitch {
+//           title
+//           description
+//         }
+//         description
+//         intro {
+//           blurbs {
+//             image {
+//               childImageSharp {
+//                 fluid(maxWidth: 240, quality: 64) {
+//                   ...GatsbyImageSharpFluid
+//                 }
+//               }
+//             }
+//             text
+//           }
+//           heading
+//           description
+//         }
+//       }
+//     }
+//   }
+// `
